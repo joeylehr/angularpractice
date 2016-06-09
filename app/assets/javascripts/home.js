f21de4ed365197562c8775f4f1b7c9c866c4821c
joeylehr <joe.c.lehr@gmail.com>
2016-06-08 22:20:55 -0400

    additional gems

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,1 +1,13 @@
 receta = angular.module('receta',[])
+
+app.config([ '$routeProvider', ($routeProvider)
+    $routeProvider
+      .when('/',
+        templateUrl: "index.html"
+        controller: 'SomeController'
+      )
+      .when('/recipes/new',
+        templateUrl: "new.html"
+        controller: 'SomeOtherController'
+      )
+])
